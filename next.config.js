63a96bb802b25ebedf0f89522b0bfa8f02644a39
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2023-04-19 08:22:18 +0300

    base configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const withAntdLess = require("next-plugin-antd-less");
+const withPlugins = require("next-compose-plugins");
 
-module.exports = nextConfig
+const nextConfig = {
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+
+  webpack(config) {
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [
+    [
+      withAntdLess,
+      {
+        modifyVars: {
+          "@primary-color": "#120a8f",
+          "@error-color": "#b91600",
+          "@layout-body-background": "#F9F9F9",
+          "@font-family": "inherit",
+          "@font-size-base": "16px",
+          "@text-color": "#1e1f1d",
+        },
+      },
+    ],
+  ],
+  nextConfig
+);
