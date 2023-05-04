992dcfcd5c3d5922e64e0ebcd4c721286eb77e5b
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2023-05-04 22:10:08 +0300

    header desktop forever

--- src/components/partials/Header/Header.tsx
+++ src/components/partials/Header/Header.tsx
@@ -1,12 +1,12 @@
 import useMedia from "@hooks/useMedia";
 import HeaderDesktop from "./HeaderDesktop";
-import HeaderMobile from "./HeaderMobile";
+// import HeaderMobile from "./HeaderMobile";
 
 const Header = () => {
   const { lessThanSM } = useMedia();
 
   const getHeader = () => {
-    if (lessThanSM) return <HeaderMobile />;
+    if (lessThanSM) return <HeaderDesktop />;
 
     return <HeaderDesktop />;
   };
