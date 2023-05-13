0c2b4746818748ceb76ad5950fe376ca8cc3b82f
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2023-05-13 18:54:27 +0300

    make bggradient more colorful

--- src/components/sections/home/IntroSection/constants.ts
+++ src/components/sections/home/IntroSection/constants.ts
@@ -1,15 +1,15 @@
 export const GRADIENTS = [
   {
-    width: "500px",
-    height: "500px",
+    width: "700px",
+    height: "700px",
     left: "10%",
     top: "10%",
     color: "#b5c9ff",
     animation: true,
   },
   {
-    width: "500px",
-    height: "500px",
+    width: "700px",
+    height: "700px",
     color: "linear-gradient(133.63deg,#fff3b5 14.35%,#c5ff8b 87.65%)",
     left: "60%",
     top: "10%",
